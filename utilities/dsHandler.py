b6b5502424cb86cb5d34e0911d7bd042b6744da7
Gaston <lazcanogaston98@gmail.com>
2023-02-13 23:35:04 -0300

    improvement in dsHandler / logs added to gitignore

--- utilities/dsHandler.py
+++ utilities/dsHandler.py
@@ -1,9 +1,17 @@
 class DsHandler():
 
+    # def read_dataset(ds):
+    #     print(f"{ds}\n")
+    #     tuples_list = []
+    #     for key in ds:
+    #         print(f"key: {key}, value: {ds[key]}\n")
+    #         tuples_list.append((key, ds[key]))
+    #     return tuples_list
+
     def read_dataset(ds):
         print(f"{ds}\n")
         tuples_list = []
-        for key in ds:
-            print(f"key: {key}, value: {ds[key]}\n")
-            tuples_list.append((key, ds[key]))
+        for key, value in ds.items():
+            print(f"key: {key}, value: {value}\n")
+            tuples_list.append((key, value))
         return tuples_list
